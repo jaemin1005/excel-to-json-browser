18c4214f0b9c7eab7010e27a8f4e545d3a57d8a7
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-21 10:38:11 +0900

    Feat | Exel로 러스트파일 읽기 작성완료

--- pkg/exel_to_json/src/lib.rs
+++ pkg/exel_to_json/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use wasm_bindgen::prelude::*;
+use calamine::{open_workbook, Reader, Xlsx};
+use std::io::Cursor;
+use serde_json::json;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+pub fn excel_to_json(excel_data: &[u8]) -> String {
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    //* Cursor는 데이터를 메모리에 버퍼로 저장하고, 이를 파일처럼 읽고 쓸 수 있도록 해줌 */
+    let cursor = Cursor::new(excel_data);
+    
+    //* 파일처럼 만들어진 curosr를 이용해 엑셀파일처럼 연다. */
+    let mut workbook = Xlsx::new(cursor).unwrap();
+
+    let mut sheet_json = vec![];
+
+    for sheet_name in workbook.sheet_names().to_owned() {
+        if let Ok(range) = workbook.worksheet_range(&sheet_name){
+            let mut rows_json = vec![];
+
+            for row in range.rows() {
+                //* ROW의 Cell 데이터들 match를 이용해 변환 */
+                let row_json: Vec<_> = row.iter().map(|cell| match cell {
+                    calamine::Data::Empty => json!(null),
+                    calamine::Data::Bool(b) => json!(b),
+                    calamine::Data::Float(f) => json!(f),
+                    calamine::Data::Int(i) => json!(i),
+                    calamine::Data::String(s) => json!(s),
+                    _ => json!(null)
+                }).collect();
+
+                rows_json.push(json!(row_json));
+            }
+
+            sheet_json.push(rows_json);
+        }
     }
+
+    json!(sheet_json).to_string()
 }
