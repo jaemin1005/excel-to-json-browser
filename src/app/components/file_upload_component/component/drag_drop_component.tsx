b12410adb5940527af7acd727377feb31f7de171
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-28 01:29:13 +0900

    Style | 드래그 방지 하였습니다

--- src/app/components/file_upload_component/component/drag_drop_component.tsx
+++ src/app/components/file_upload_component/component/drag_drop_component.tsx
@@ -6,8 +6,8 @@ export function DragDropComponent() {
       <div className="w-[120px] h-[120px] rounded-full bg-[#4AB2D3] relative flex justify-center items-center">
         <Image src="/cloud_upload.svg" width={80} height={80} alt="" objectFit="contain"/>
       </div>
-      <h1 className="font-inter text-[40px] font-extrabold text-zinc-600">Drag & Drop</h1>
-      <h3 className="font-inter text-[20px] line-[24px] font-extrabold text-[#B4B0B0]">Only Xlsx up to 10mb</h3>
+      <h1 className="select-none font-inter text-[40px] font-extrabold text-zinc-600">Drag & Drop</h1>
+      <h3 className="select-none font-inter text-[20px] line-[24px] font-extrabold text-[#B4B0B0]">Only Xlsx up to 10mb</h3>
     </div>
   );
 }
