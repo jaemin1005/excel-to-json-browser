5508cc59c65f8aad2016b0b82a75985ec2dbcf48
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-28 01:18:43 +0900

    New | 브라우저단에서 xlsx파일을 변환하고 압축된 상태로 변환하는 함수이다

---
+++ src/utils/transfer_json.ts
@@ -0,0 +1,20 @@
+import init, { all_excel_to_json } from "./wasm/excel_to_json";
+import { saveAs } from 'file-saver';
+import JSZip from "jszip";
+
+export async function transferJSON(fileName: string, byteArray: Uint8Array) {
+  await init();
+  const jsonObjs = all_excel_to_json(byteArray, true);
+
+  const zip = new JSZip();
+
+  jsonObjs.forEach((obj, idx) =>
+    zip.file(`${fileName}_${idx}.json`, JSON.stringify(obj, null, 2))
+  );
+
+  zip.generateAsync({ type: "blob" })
+  .then(function(content) {
+    // 6. 파일을 다운로드합니다.
+    saveAs(content, "data.zip");
+  });
+}
