8d99f574f9e06583f01681b2158063ea43a28b5c
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-28 01:26:36 +0900

    Feat | nextui추가로인한 변경사항

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|card|image|progress|popover|ripple|spinner).js",
+    "./node_modules/@nextui-org/theme/dist/components/(button|card|image|progress|spinner|popover|ripple).js"
   ],
   theme: {
     extend: {
