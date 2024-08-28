3fa592f16ac98e93710a59d9f67f965405845c43
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-28 18:05:52 +0900

    Feat | 그리드 방식으로 변경하였습니다

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 "use client";
 import { FileUploadComponent } from "./components/file_upload_component/file_upload_component";
 
-
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <FileUploadComponent />
+    <div className="w-screen grid grid-rows-3-[1fr_3fr_1fr] lg:grid-cols-[1fr_3fr_1fr] grid-cols-2 h-screen gap-2">
+      <div className="row-start-1 lg:col-start-2 col-start-1">
+        {/* 상단 광고 */}
+        </div>
+      <div className="lg:row-start-2 lg:col-start-1 row-start-1 col-start-2">
+        {/* 좌측 광고 */}
+      </div>
+      <div className="row-start-2 lg:col-start-2border lg:col-span-1 col-span-2 p-4 flex justify-center items-center">
+        <FileUploadComponent />
+      </div>
+      <div className="lg:row-start-2 lg:col-start-3 row-start-3 col-start-2">
+        {/* 우측 광고 */}
+      </div>
+      <div className="row-start-3 lg:col-start-2 col-start-1">
+        {/* 하단 광고 */}
+        </div>
     </div>
   );
+
+  // return (
+
+  //   <div className="flex flex-col justify-center items-center h-full w-full">
+  //     <FileUploadComponent />
+  //   </div>
+  // );
 }
