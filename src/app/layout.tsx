5e0d505ab481079660c993cf5d13c032ecd003e3
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-28 18:05:26 +0900

    Feat | meta데이터 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { FileContextProvider } from "./components/file_upload_component/context/file_list_context";
+import { Meta as META } from "../static/meta";
 
 export const metadata: Metadata = {
-  title: "XLSX-JSON",
-  description: "Converted XLSX To JSON",
+  metadataBase: META.METADATABASE,
+  title: META.TITLE,
+  description: META.DESCRIPTION,
+  keywords: META.KEY_WORDS,
+  authors: { name: "Jae Min" },
+  robots: META.ROBOTS,
 };
 
 export default function RootLayout({
@@ -14,8 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+      </head>
       <FileContextProvider>
-        <body className="bg-[#E9E0E0] w-screen h-screen flex flex-col justify-center items-center">
+        <body className="bg-[#E9E0E0] w-screen h-screen flex flex-col justify-center items-center overscroll-y-none">
           {children}
         </body>
       </FileContextProvider>
