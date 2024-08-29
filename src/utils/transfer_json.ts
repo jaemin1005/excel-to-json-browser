c08d358d70670666dbdfccda896bca8e16a4626f
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-29 21:49:52 +0900

    Feat | 확장자 명에 따라 json으로 변환하는 로직을 분기하였습니다

--- src/utils/transfer_json.ts
+++ src/utils/transfer_json.ts
@@ -1,20 +1,28 @@
-import init, { all_excel_to_json } from "./wasm/excel_to_json";
-import { saveAs } from 'file-saver';
+import { FILE_EXTENSION } from "@/static/file_extension";
+import init, { all_excel_to_json, csv_to_json } from "./wasm/excel_to_json";
+import { saveAs } from "file-saver";
 import JSZip from "jszip";
 
-export async function transferJSON(fileName: string, byteArray: Uint8Array) {
+export async function transferJSON(
+  fileName: [string, string],
+  byteArray: Uint8Array
+) {
   await init();
-  const jsonObjs = all_excel_to_json(byteArray, true);
-
   const zip = new JSZip();
 
-  jsonObjs.forEach((obj, idx) =>
-    zip.file(`${fileName}_${idx}.json`, JSON.stringify(obj, null, 2))
-  );
+  if (fileName[1] === FILE_EXTENSION.XLSX)
+    all_excel_to_json(byteArray, true).forEach((obj, idx) =>
+      zip.file(`${fileName[0]}_${idx}.json`, JSON.stringify(obj, null, 2))
+    );
+  else {
+    zip.file(
+      `${fileName[0]}.json`,
+      JSON.stringify(csv_to_json(byteArray), null, 2)
+    );
+  }
 
-  zip.generateAsync({ type: "blob" })
-  .then(function(content) {
+  zip.generateAsync({ type: "blob" }).then(function (content) {
     // 6. 파일을 다운로드합니다.
-    saveAs(content, `${fileName}.zip`);
+    saveAs(content, `${fileName[0]}.zip`);
   });
 }
