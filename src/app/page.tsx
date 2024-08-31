d8183f412174c3918b46d0bfb017a403990647bc
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-31 20:23:00 +0900

    New | AdSense컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,36 @@
 "use client";
+import { useEffect } from "react";
 import { FileUploadComponent } from "./components/file_upload_component/file_upload_component";
+import dynamic from "next/dynamic";
+
+const AdSense = dynamic(() => import("./components/ad_sense"), { ssr: false });
 
 export default function Home() {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error("AdSense error:", e);
+    }
+  }, []);
+
   return (
-    <div className="w-screen grid grid-rows-3-[1fr_3fr_1fr] lg:grid-cols-[1fr_3fr_1fr] grid-cols-2 h-screen gap-2">
+    <div className="w-screen grid grid-rows-3-[1fr_3fr_1fr] lg:grid-cols-[1fr_3fr_1fr] grid-cols-1 h-screen gap-2">
       <div className="row-start-1 lg:col-start-2 col-start-1">
-        {/* 상단 광고 */}
-        </div>
-      <div className="lg:row-start-2 lg:col-start-1 row-start-1 col-start-2">
+        <AdSense ad_slot="2125820751" />
+      </div>
+      <div className="lg:row-start-2 lg:col-start-1 lg:block hidden">
         {/* 좌측 광고 */}
       </div>
       <div className="row-start-2 lg:col-start-2border lg:col-span-1 col-span-2 p-4 flex justify-center items-center">
         <FileUploadComponent />
       </div>
-      <div className="lg:row-start-2 lg:col-start-3 row-start-3 col-start-2">
+      <div className="lg:row-start-2 lg:col-start-3 row-start-3 col-start-2 lg:block hidden">
         {/* 우측 광고 */}
       </div>
       <div className="row-start-3 lg:col-start-2 col-start-1">
-        {/* 하단 광고 */}
-        </div>
+        <AdSense ad_slot="2234466117" />
+      </div>
     </div>
   );
 
