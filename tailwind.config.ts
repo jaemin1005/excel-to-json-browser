85d1a39daed2f429dd1612866d6429c9718be3f5
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-09 21:36:10 +0900

    Npm | nextui-popover, react-icon 설치
    nextui-popover로 사용자에게 주의점을 알려준다

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,13 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|card|image|progress|spinner|popover|ripple).js"
+    "./node_modules/@nextui-org/theme/dist/components/(button|card|image|popover|progress|spinner|ripple).js",
   ],
   theme: {
     extend: {
       backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(180deg, rgb(121,9,69) 0%, rgb(104,37,95) 39%, rgb(2,0,36) 100%)",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
@@ -40,7 +42,7 @@ const config: Config = {
       animation: {
         "expand-to-original": "expandToOriginal 2s ease-in-out forwards",
         "zoom-in": "zoomin 0.6s ease-out",
-        "jiggle": "jiggle 0.5s ease-in-out",
+        jiggle: "jiggle 0.5s ease-in-out",
       },
     },
   },
